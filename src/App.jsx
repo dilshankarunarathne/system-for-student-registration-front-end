8300af2188f3aaba49475ba758439e535be6b45f
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-24 15:53:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<h1 className="text-3xl font-bold underline">Hello world!</h1>} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
