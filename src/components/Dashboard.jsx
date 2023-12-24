7d2193c08a7615bd06a4f7727bef83c44bee85c0
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-24 17:01:04 +0530

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -6,6 +6,11 @@ const Dashboard = () => {
   const courses = [
     { courseCode: 'CS101', academicYear: '2021-2022' },
     { courseCode: 'CS102', academicYear: '2021-2022' },
+    { courseCode: 'CS103', academicYear: '2021-2022' },
+    { courseCode: 'CS104', academicYear: '2021-2022' },
+    { courseCode: 'CS105', academicYear: '2021-2022' },
+    { courseCode: 'CS106', academicYear: '2021-2022' },
+    { courseCode: 'CS107', academicYear: '2021-2022' },
   ];
 
   return (
