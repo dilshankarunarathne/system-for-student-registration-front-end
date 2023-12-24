c8d5ce62facc7af434506e7000b51d9bca959e44
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-24 16:14:35 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,49 @@
-function Login() {
-    return <h2>Login Page</h2>;
-}
+import { useState } from 'react';
+
+const Login = () => {
+    const [email, setEmailname] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleEmailChange = (e) => {
+        setEmailname(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Email: ', email);
+        console.log('Password: ', password);
+    };
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="text"
+                        id="email"
+                        value={email}
+                        onChange={handleEmailChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={handlePasswordChange}
+                    />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+};
 
 export default Login;
