73fd5298e7a6a14c18da09a347109e3c7d733f7c
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-24 17:36:44 +0530

    Update Course.jsx

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -13,6 +13,16 @@ const Course = () => {
             <h2 className='text-course-info'>Lecturer: Mr. Some Guy</h2>
             <h2 className='text-course-info'>Credits: {id}</h2>
         </div>
+
+        <div className='flex flex-wrap justify-center items-start lecturer-login-form'>
+            <form className='flex flex-wrap justify-center items-start'>
+                <h3 className='text-2xl font-bold'>Lecturer Login</h3>
+                <input type='text' placeholder='Username' className='p-2 m-2 border-2 rounded-lg' />
+                <input type='password' placeholder='Password' className='p-2 m-2 border-2 rounded-lg' />
+                <button className='text-2xl font-bold bg-blue-500 text-white p-2 rounded-lg'>Login</button>
+                <button className='text-2xl font-bold bg-blue-500 text-white p-2 rounded-lg'>Register</button>
+            </form>
+        </div>
     </div>
   );
 };
