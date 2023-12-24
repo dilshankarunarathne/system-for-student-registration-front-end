039cd21754e48756d0a85362f9b8a48d0ef66b3e
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-24 16:14:37 +0530

    Create Register.jsx

---
+++ src/components/Register.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+
+const Register = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Name:
+                <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </label>
+            <br />
+            <label>
+                Email:
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </label>
+            <br />
+            <label>
+                Password:
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </label>
+            <br />
+            <button type="submit">Register</button>
+        </form>
+    );
+};
+
+export default Register;
