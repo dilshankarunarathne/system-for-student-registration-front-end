3867b0cba2a848774612e532494e4e40925cae92
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-24 18:00:29 +0530

    Update StudentTile.jsx

---
+++ src/widgets/StudentTile.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import './StudentTile.css';
+
+CourseTile.propTypes = {
+    id: PropTypes.string.isRequired,
+};
+
+function CourseTile({ id }) {
+    return (
+        <div className="w-64 h-64 border-2 border-black flex flex-col justify-center items-center m-4 tile">
+            <h2 className="text-2xl course-code tile-text">{id}</h2>
+        </div>
+    );
+}
+
+export default CourseTile;
