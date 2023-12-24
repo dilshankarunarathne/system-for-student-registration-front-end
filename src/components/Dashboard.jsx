ee9f71eb152b6e0db94732df4ebc13ccf2ddd9cc
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-24 17:22:02 +0530

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,17 +1,19 @@
+
+
 import CourseTile from '../widgets/CourseTile';
 
 import './Dashboard.css';
 
 const Dashboard = () => {
-  const courses = [
-    { courseCode: 'CS101', academicYear: '2021-2022' },
-    { courseCode: 'CS102', academicYear: '2021-2022' },
-    { courseCode: 'CS103', academicYear: '2021-2022' },
-    { courseCode: 'CS104', academicYear: '2021-2022' },
-    { courseCode: 'CS105', academicYear: '2021-2022' },
-    { courseCode: 'CS106', academicYear: '2021-2022' },
-    { courseCode: 'CS107', academicYear: '2021-2022' },
-  ];
+	const courses = [
+		{ id: 1, courseCode: 'CS101', academicYear: '2021-2022' },
+		{ id: 2, courseCode: 'CS102', academicYear: '2021-2022' },
+		{ id: 3, courseCode: 'CS103', academicYear: '2021-2022' },
+		{ id: 4, courseCode: 'CS104', academicYear: '2021-2022' },
+		{ id: 5, courseCode: 'CS105', academicYear: '2021-2022' },
+		{ id: 6, courseCode: 'CS106', academicYear: '2021-2022' },
+		{ id: 7, courseCode: 'CS107', academicYear: '2021-2022' },
+	];
 
   return (
     <div className='flex flex-wrap justify-center items-start main'>
