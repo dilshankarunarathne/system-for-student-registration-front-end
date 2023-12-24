8223a1005bbdabe9a1539e67175ca0dab85fdc91
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-24 16:16:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import './App.css';
+
 import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
         <Route path="/" element={<h1 className="text-3xl font-bold underline">Hello world!</h1>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
