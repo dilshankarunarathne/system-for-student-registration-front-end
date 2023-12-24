d7b100a8a6be15308d5993552c14ff4d172a51d9
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-24 17:08:41 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav className="flex justify-between items-center bg-blue-500 p-6">
-      <div className="text-white text-2xl">My App</div>
+      <div className="text-white text-2xl">Student Registration System</div>
       <div>
         <Link to="/dashboard" className="text-white mx-2">Dashboard</Link>
         <Link to="/login" className="text-white mx-2">Login</Link>
