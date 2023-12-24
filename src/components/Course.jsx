9537b392703232bfd1116310cca23f7c5db4695a
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-24 17:27:17 +0530

    Update Course.jsx

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -6,7 +6,7 @@ const Course = () => {
   return (
     <div>
         <div className="text-3xl font-bold course-info">
-            <h2>Course Code: CS101</h2>
+            <h2>Course {id} Code: CS101</h2>
             <h2>Academic Year: 2021-2022</h2>
             <h2>Lecturer: Mr. Some Guy</h2>
         </div>
