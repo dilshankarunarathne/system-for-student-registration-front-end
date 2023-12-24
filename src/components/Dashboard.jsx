9235358324136a3b6c02ad095cbde98bafeef7bc
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-24 16:49:56 +0530

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,18 @@
+import CourseTile from './CourseTile';
+
 const Dashboard = () => {
-    return (
-        <div className="flex flex-col items-center justify-center h-screen bg-gray-200">
-            
-        </div>
-    );
+  const courses = [
+    { courseCode: 'CS101', academicYear: '2021-2022' },
+    { courseCode: 'CS102', academicYear: '2021-2022' },
+  ];
+
+  return (
+    <div className="flex flex-wrap justify-center items-start h-screen bg-gray-200 p-4">
+      {courses.map((course, index) => (
+        <CourseTile key={index} courseCode={course.courseCode} academicYear={course.academicYear} />
+      ))}
+    </div>
+  );
 };
 
 export default Dashboard;
