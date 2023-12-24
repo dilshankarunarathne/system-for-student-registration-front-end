e1bbd88c2d84164f753f41f9b29f7de87adb19e5
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-24 17:28:48 +0530

    Update Course.jsx

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -6,9 +6,10 @@ const Course = () => {
   return (
     <div>
         <div className="text-3xl font-bold course-info">
-            <h2>Course {id} Code: CS101</h2>
-            <h2>Academic Year: 2021-2022</h2>
-            <h2>Lecturer: Mr. Some Guy</h2>
+            <h2 className='text-course-info'>Course Code: CS101</h2>
+            <h2 className='text-course-info'>Academic Year: 2021-2022</h2>
+            <h2 className='text-course-info'>Lecturer: Mr. Some Guy</h2>
+            <h2 className='text-course-info'>Credits: {id}</h2>
         </div>
     </div>
   );
