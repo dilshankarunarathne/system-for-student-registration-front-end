eff352f3bca319b6f54913ed2e54c980abf21d65
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-25 08:40:42 +0530

    Update Student.jsx

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -99,7 +99,16 @@ const Student = () => {
                 </table>
             </div>
             <div className='flex flex-wrap justify-center items-start student-data-stats'>
-                
+                <Pie
+                    data={{
+                        labels: ['Red', 'Blue', 'Yellow'],
+                        datasets: [{
+                            data: [300, 50, 100],
+                            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+                            hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
+                        }]
+                    }}
+                />
             </div>
         </div>
     </div>
