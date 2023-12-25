9e620b62e55f2d32deb5a2a929220baebc2c70eb
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-25 09:23:48 +0530

    Update MarkAttendance.jsx

---
+++ src/components/MarkAttendance.jsx
@@ -0,0 +1,22 @@
+import { useParams } from 'react-router-dom';
+
+import './MarkAttendance.css';
+
+const MarkAttendance = () => {
+  const { id } = useParams();
+
+  return (
+    <div>
+        <div className="text-3xl font-bold student-info">
+            <h2 className='text-course-info'>Name: Mr. Some Student</h2>
+            <h2 className='text-course-info'>Registration Number : EUTC/2019/COM/24</h2>
+            <h2 className='text-course-info'>Academic Year: 2021-2022</h2>
+            <h2 className='text-course-info'>GPA: {id}</h2>
+        </div>
+
+        
+    </div>
+  );
+};
+
+export default MarkAttendance;
