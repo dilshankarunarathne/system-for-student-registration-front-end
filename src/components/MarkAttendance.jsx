ce0110ea9640cd606b228ba9c4ffabd5283aa385
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-25 12:22:10 +0530

    Update MarkAttendance.jsx

--- src/components/MarkAttendance.jsx
+++ src/components/MarkAttendance.jsx
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom';
 
+
 import './MarkAttendance.css';
 
 const MarkAttendance = () => {
@@ -15,7 +16,7 @@ const MarkAttendance = () => {
         </div>
 
         <div className='flex justify-center camera-container'>
-          
+
         </div>
     </div>
   );
