aed0630c23ecf54df1c8ba95107fecabc3cc10a8
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 00:36:12 +0530

    Update AppRoutes.jsx

---
+++ src/AppRoutes.jsx
@@ -0,0 +1,29 @@
+import { Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import LecturerLogin from './components/LecturerLogin';
+import Register from './components/Register';
+import Dashboard from './components/Dashboard';
+import Course from './components/Course';
+import Student from './components/Student';
+import MarkAttendance from './components/MarkAttendance';
+import Navbar from './components/Navbar';
+
+const AppRoutes = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/course/:id" element={<Course />} />
+        <Route path="/student/:id" element={<Student />} />
+        <Route path="/mark-attendance/:id" element={<MarkAttendance />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/lecturer-login" element={<LecturerLogin />} />
+      </Routes>
+    </>
+  );
+};
+
+export default AppRoutes;
