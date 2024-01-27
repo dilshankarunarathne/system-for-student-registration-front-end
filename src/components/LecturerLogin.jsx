8f2f56e977c19f89011bc6b391ade1374630ef05
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-27 23:57:13 +0530

    Update LecturerLogin.jsx

--- src/components/LecturerLogin.jsx
+++ src/components/LecturerLogin.jsx
@@ -1,11 +1,13 @@
 import { useParams, Link } from 'react-router-dom';
+import { useState } from 'react';
 
 import StudentTile from '../widgets/StudentTile';
 
-import './LecturerLogin.css';
+import './Course.css';
 
 const LecturerLogin = () => {
   const { id } = useParams();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const students = [
     { id: 1, registration_number: 'EUTC/2019/COM/24', name: 'John Doe' },
@@ -16,6 +18,11 @@ const LecturerLogin = () => {
     { id: 6, registration_number: 'EUTC/2019/COM/63', name: 'Karen Doe' },*/
   ];
 
+  const handleLoginClick = (e) => {
+    e.preventDefault();
+    setIsLoggedIn(true);
+  };
+
   return (
     <div>
         <div className="text-3xl font-bold course-info">
@@ -25,13 +32,31 @@ const LecturerLogin = () => {
             <h2 className='text-course-info'>Credits: {id}</h2>
         </div>
 
-        <div className='flex flex-wrap justify-center items-start student-tiles'>
-            {students.map((student) => (
-                <Link to={`/student/${student.id}`} key={student.id}>
-                    <StudentTile registration_number={student.registration_number} name={student.name} />
-                </Link>
-            ))}
+        {!isLoggedIn && (
+        <div className='flex flex-wrap justify-center items-start lecturer-login-form'>
+            <form className='flex flex-wrap justify-center items-start'>
+            <div className='flex flex-wrap justify-center items-start login-form-half'>
+                <h3 className='text-2xl font-bold login-form-content'>Lecturer Login</h3>
+                <input type='text' placeholder='Username' className='p-2 m-2 border-2 rounded-lg login-form-content' />
+                <input type='password' placeholder='Password' className='p-2 m-2 border-2 rounded-lg login-form-content' />
+            </div>
+            <div className='flex flex-wrap justify-center items-start login-form-half'>
+                <button onClick={handleLoginClick} className='text-2xl font-bold bg-blue-500 text-white p-2 rounded-lg login-form-content'>Login</button>
+                <button className='text-2xl font-bold bg-blue-500 text-white p-2 rounded-lg login-form-content'>Register</button>
+            </div>
+            </form>
         </div>
+        )}
+
+        {isLoggedIn && (
+            <div className='flex flex-wrap justify-center items-start student-tiles'>
+                {students.map((student) => (
+                    <Link to={`/student/${student.id}`} key={student.id}>
+                        <StudentTile registration_number={student.registration_number} name={student.name} />
+                    </Link>
+                ))}
+            </div>
+        )}
     </div>
   );
 };
