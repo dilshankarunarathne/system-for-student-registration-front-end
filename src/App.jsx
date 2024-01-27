745acd253a4fb177b8c2fe0cdfc8795602a2d27c
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 00:01:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
         <Route path="/mark-attendance/:id" element={<MarkAttendance />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/lecturer-login" element={<Register />} />
+        <Route path="/lecturer-login" element={<LecturerLogin />} />
       </Routes>
     </Router>
   );
