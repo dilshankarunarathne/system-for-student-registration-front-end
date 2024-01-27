ee7492a2358f67255cffb11801a35f483a6a9b1c
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 00:58:23 +0530

    Update CourseSetup.jsx

--- src/components/CourseSetup.jsx
+++ src/components/CourseSetup.jsx
@@ -1,4 +1,4 @@
-import { useParams, Link, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { useContext, useEffect } from 'react';
 
 import { AuthContext } from '../auth/AuthContext.jsx';
