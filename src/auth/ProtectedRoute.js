aecbad7d127a12d4dda87aabe0dbda5f87f06424
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 00:26:33 +0530

    Update ProtectedRoute.js

--- src/auth/ProtectedRoute.js
+++ src/auth/ProtectedRoute.js
@@ -2,9 +2,11 @@ import { Route, Navigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { AuthContext } from './AuthContext';
 
+// eslint-disable-next-line react/prop-types
 const ProtectedRoute = ({ roles, ...props }) => {
   const { role } = useContext(AuthContext);
 
+  // eslint-disable-next-line react/prop-types
   if (roles.includes(role)) {
     return <Route {...props} />;
   } else {
