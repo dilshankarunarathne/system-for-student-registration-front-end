0e7a4a6c3550a0f48a1d6a947429f29897ad4dbe
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 00:58:04 +0530

    Update CourseSetup.jsx

---
+++ src/components/CourseSetup.jsx
@@ -0,0 +1,48 @@
+import { useParams, Link, useNavigate } from 'react-router-dom';
+import { useContext, useEffect } from 'react';
+
+import { AuthContext } from '../auth/AuthContext.jsx';
+
+import StudentTile from '../widgets/StudentTile';
+
+import './CourseSetup.css';
+
+const CourseSetup = () => {
+    const navigate = useNavigate();
+    const { id } = useParams();
+    const { isLoggedIn } = useContext(AuthContext);
+ 
+  const students = [
+    { id: 1, registration_number: 'EUTC/2019/COM/24', name: 'John Doe' },
+    { id: 2, registration_number: 'EUTC/2019/COM/10', name: 'Jane Doe' },
+    { id: 3, registration_number: 'EUTC/2019/COM/60', name: 'John Smith' },
+    { id: 4, registration_number: 'EUTC/2019/COM/74', name: 'Jane Smith' },
+    /*{ id: 5, registration_number: 'EUTC/2019/COM/57', name: 'Simon Doe' },
+    { id: 6, registration_number: 'EUTC/2019/COM/63', name: 'Karen Doe' },*/
+  ];
+
+  useEffect(() => {
+    return () => {
+      if (!isLoggedIn) {
+        navigate('/lecturer-login');
+      }
+    };
+  }, [isLoggedIn, navigate]);
+
+  return (
+    <div>
+        <div className="text-3xl font-bold course-info">
+            <h2 className='text-course-info'>Course Code: CS101</h2>
+            <h2 className='text-course-info'>Academic Year: 2021-2022</h2>
+            <h2 className='text-course-info'>Lecturer: Mr. Some Guy</h2>
+            <h2 className='text-course-info'>Credits: {id}</h2>
+        </div>
+
+        <div className='flex flex-wrap justify-center items-start'>
+            
+        </div>
+    </div>
+  );
+};
+
+export default CourseSetup;
