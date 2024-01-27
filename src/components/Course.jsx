6d8f6cf23f5beea30672fd6f600d570470ee5c80
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 00:03:58 +0530

    Update Course.jsx

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,4 +1,4 @@
-import { useParams, Link, useHistory } from 'react-router-dom';
+import { useParams, Link, useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 
 import StudentTile from '../widgets/StudentTile';
