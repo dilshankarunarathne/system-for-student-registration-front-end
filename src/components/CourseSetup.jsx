d7fb4f54ff62a856519e0e265edc5f19e3402555
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 08:40:08 +0530

    Update CourseSetup.jsx

--- src/components/CourseSetup.jsx
+++ src/components/CourseSetup.jsx
@@ -105,7 +105,7 @@ const CourseSetup = () => {
           <div style={{ flexDirection: 'column', display: 'flex' }}>
           <button className="new-lecture-button text-2xl mb-4 font-bold bg-blue-500 text-white p-2 rounded-lg" onClick={handleStartLectureMark}>Mark Start Lecture</button>
           <button className="edit-course-button text-2xl font-bold bg-blue-500 text-white p-2 rounded-lg" onClick={handleEndLectureMark}>Mark End Lecture</button>
-          
+          </div>
         </div>
 
         {showPopupLecture && (
