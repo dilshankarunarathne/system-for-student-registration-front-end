48a70f18550fdb6af1bd42437910e33c036e50af
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 07:16:39 +0530

    Update CourseSetup.jsx

--- src/components/CourseSetup.jsx
+++ src/components/CourseSetup.jsx
@@ -18,6 +18,10 @@ const CourseSetup = () => {
     };
   }, [isLoggedIn, navigate]);
 
+  const handleNewLecture = () => {
+    navigate('/mark-attendance');
+  }
+
   return (
     <div>
         <div className="text-3xl font-bold course-info">
@@ -28,7 +32,7 @@ const CourseSetup = () => {
         </div>
 
         <div className='flex flex-wrap justify-center items-start lec-controls'>
-          <button className="new-lecture-button">New Lecture</button>
+          <button className="new-lecture-button" onClick={handleNewLecture}>New Lecture</button>
           <button className="edit-course-button">Edit Course Details</button>
         </div>
     </div>
