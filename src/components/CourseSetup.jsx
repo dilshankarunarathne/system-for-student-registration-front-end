8ecaa7d2acea7d02f1e11692b26c496d3a5e0c6c
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 08:20:24 +0530

    Update CourseSetup.jsx

--- src/components/CourseSetup.jsx
+++ src/components/CourseSetup.jsx
@@ -63,7 +63,7 @@ const CourseSetup = () => {
     <div>
         <div className="text-3xl font-bold course-info">
             <h2 className='text-course-info'>Course Code: CS101</h2>
-            <h2 className='text-course-info'>Academic Year: 2021-2022</h2>
+            <h2 className='text-course-info'>Academic Year: 2021-2022</h2> 
             <h2 className='text-course-info'>Lecturer: Mr. Some Guy</h2>
             <h2 className='text-course-info'>Credits: {id}</h2>
         </div>
