f79aa7c797b8800072a1e1245c6a02c30ff40c02
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 09:47:28 +0530

    Update MarkAttendanceList.jsx

--- src/components/MarkAttendanceList.jsx
+++ src/components/MarkAttendanceList.jsx
@@ -69,11 +69,7 @@ const MarkAttendanceList = () => {
         </div>
 
         <div className='flex flex-wrap justify-center items-start mark-controls'>
-          <button className="new-lecture-button text-2xl font-bold bg-blue-500 text-white p-2 rounded-lg" onClick={handleNewLecture}>New Lecture</button>
-          <button className="edit-course-button text-2xl ml-3 font-bold bg-blue-500 text-white p-2 rounded-lg" onClick={editCourseDetails}>Edit Course Details</button>
-          <button className="edit-course-button text-2xl ml-3 font-bold bg-blue-500 text-white p-2 rounded-lg" onClick={editCourseDetails}>Print Full Report</button> {/* TODO */}
-          <button className="edit-course-button text-2xl ml-3 font-bold bg-blue-500 text-white p-2 rounded-lg" onClick={editCourseDetails}>Reset All Data</button>
-          <button className="edit-course-button text-2xl ml-3 font-bold bg-blue-500 text-white p-2 rounded-lg" onClick={handleStudentsListClick}>Students List</button>
+          <button className="new-lecture-button text-2xl font-bold bg-blue-500 text-white p-2 rounded-lg" onClick={handleFinishMark}>New Lecture</button>
         </div>
 
         <div className='flex flex-wrap justify-center items-start student-tiles'>
