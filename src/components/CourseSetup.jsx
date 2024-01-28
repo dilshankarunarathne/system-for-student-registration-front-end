036c2e2834e3af203c3c13d92ca2aaa072f98b66
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 08:03:01 +0530

    Update CourseSetup.jsx

--- src/components/CourseSetup.jsx
+++ src/components/CourseSetup.jsx
@@ -66,7 +66,7 @@ const CourseSetup = () => {
             </label>
             <br/><br/>
             <button type='submit' className='text-l mr-3 font-bold bg-blue-500 text-white p-2 rounded-lg'>Save</button>
-            <button onClick={closePopup} className='text-l ml-3 font-bold bg-blue-500 text-white p-2 rounded-lg'>Close</button>
+            <button onClick={closePopupLecture} className='text-l ml-3 font-bold bg-blue-500 text-white p-2 rounded-lg'>Close</button>
           </form>
         </div>
       )}
