340d8bd1f9a267a35051b0ea0c5661f284742c19
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 08:48:13 +0530

    Update MarkAttendance.jsx

--- src/components/MarkAttendance.jsx
+++ src/components/MarkAttendance.jsx
@@ -13,7 +13,7 @@ const MarkAttendance = () => {
             <h2 className='text-course-info'>Registration Number : EUTC/2019/COM/24</h2>
             <h2 className='text-course-info'>Academic Year: 2021-2022</h2>
             <h2 className='text-course-info'>GPA: {id}</h2>
-            <h2 className='text-course-info'>GPA: {id}</h2>
+            <h2 className='text-course-info'>Lecture ID: {id}</h2>
         </div>
 
         <div className='flex justify-center camera-container'>
