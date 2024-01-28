3e5c37bfabf171d0f06ca0e3cd0d3c098ec70da4
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 09:44:22 +0530

    Update AppRoutes.jsx

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -20,7 +20,7 @@ const AppRoutes = () => {
         <Route path="/course/:id" element={<Course />} />
         <Route path="/student/:id" element={<Student />} />
         <Route path="/mark-attendance/:studId/:lecId" element={<MarkAttendance />} />
-        <Route path='/course-attendance-mark/:id' element={<MarkAttendanceList />} />
+        {/* <Route path='/course-attendance-mark/:id' element={<MarkAttendanceList />} /> */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/lecturer-login" element={<LecturerLogin />} />
