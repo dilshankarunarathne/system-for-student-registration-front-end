7bec886db37d96a3d79eb45bd5c7e72e302bc34b
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 08:24:12 +0530

    Update CourseSetup.jsx

--- src/components/CourseSetup.jsx
+++ src/components/CourseSetup.jsx
@@ -76,12 +76,12 @@ const CourseSetup = () => {
             <h2 className='text-course-info'>Credits: {id}</h2>
         </div>
 
-        <div className='flex flex-wrap justify-center mt-4 items-start lec-controls'>
-          <button className="new-lecture-button text-2xl mr-3 font-bold bg-blue-500 text-white p-2 rounded-lg" onClick={handleNewLecture}>New Lecture</button>
-          <button className="edit-course-button text-2xl ml-3 font-bold bg-blue-500 text-white p-2 rounded-lg" onClick={editCourseDetails}>Edit Course Details</button>
-          <button className="edit-course-button text-2xl ml-3 font-bold bg-blue-500 text-white p-2 rounded-lg" onClick={editCourseDetails}>Print Full Report</button> {/* TODO */}
-          <button className="edit-course-button text-2xl ml-3 font-bold bg-blue-500 text-white p-2 rounded-lg" onClick={editCourseDetails}>Reset All Data</button>
-          <button className="edit-course-button text-2xl ml-3 font-bold bg-blue-500 text-white p-2 rounded-lg" onClick={handleStudentsListClick}>Students List</button>
+        <div className='flex flex-wrap justify-center items-start lec-controls'>
+          <button className="new-lecture-button text-2xl mr-3 mt-4 font-bold bg-blue-500 text-white p-2 rounded-lg" onClick={handleNewLecture}>New Lecture</button>
+          <button className="edit-course-button text-2xl ml-3 mt-4 font-bold bg-blue-500 text-white p-2 rounded-lg" onClick={editCourseDetails}>Edit Course Details</button>
+          <button className="edit-course-button text-2xl ml-3 mt-4 font-bold bg-blue-500 text-white p-2 rounded-lg" onClick={editCourseDetails}>Print Full Report</button> {/* TODO */}
+          <button className="edit-course-button text-2xl ml-3 mt-4 font-bold bg-blue-500 text-white p-2 rounded-lg" onClick={editCourseDetails}>Reset All Data</button>
+          <button className="edit-course-button text-2xl ml-3 mt-4 font-bold bg-blue-500 text-white p-2 rounded-lg" onClick={handleStudentsListClick}>Students List</button>
         </div>
 
         <div className='flex flex-wrap justify-center items-start current-lecture-controls'>
