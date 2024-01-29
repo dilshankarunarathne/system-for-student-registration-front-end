3dd3b6bf0b09e7a39480041c1cbb2a3d31db92f7
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-29 08:14:40 +0530

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,32 +1,31 @@
-import { Link } from 'react-router-dom';
+import { Link, useEffect, useState } from 'react-router-dom';
 
 import CourseTile from '../widgets/CourseTile';
 
 import './Dashboard.css';
 
 const Dashboard = () => {
-	const courses = [
-		{ id: 1, courseCode: 'CS101', academicYear: '2021-2022' },
-		{ id: 2, courseCode: 'CS102', academicYear: '2021-2022' },
-		{ id: 3, courseCode: 'CS103', academicYear: '2021-2022' },
-		{ id: 4, courseCode: 'CS104', academicYear: '2021-2022' },
-		/*{ id: 5, courseCode: 'CS105', academicYear: '2021-2022' },
-		{ id: 6, courseCode: 'CS106', academicYear: '2021-2022' },
-		{ id: 7, courseCode: 'CS107', academicYear: '2021-2022' },*/
-	];
+    const [courses, setCourses] = useState([]);
 
-  return (
-    <div className='flex flex-wrap justify-center items-start main'>
-		<h1 className="text-3xl font-bold title">Courses</h1>
-		<div className="flex flex-wrap justify-center h-screen p-4 courses">
-			{courses.map((course) => (
-				<Link to={`/course/${course.id}`} key={course.id}>
-					<CourseTile courseCode={course.courseCode} academicYear={course.academicYear} />
-				</Link>
-			))}
+    useEffect(() => {
+        fetch('http://127.0.0.1:8000/api/course/get-all')
+        .then(response => response.json())
+        .then(data => setCourses(data.data))
+        .catch(error => console.error('There was an error!', error));
+    }, []);
+
+    return (
+        <div className='flex flex-wrap justify-center items-start main'>
+            <h1 className="text-3xl font-bold title">Courses</h1>
+            <div className="flex flex-wrap justify-center h-screen p-4 courses">
+                {courses.map((course) => (
+                    <Link to={`/course/${course.id}`} key={course.id}>
+                        <CourseTile courseCode={course.code} academicYear={course.academic_year} />
+                    </Link>
+                ))}
+            </div>
         </div>
-    </div>
-  );
+    );
 };
 
 export default Dashboard;
