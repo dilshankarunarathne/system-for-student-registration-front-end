3296f2bb64197b155744fd4fb47f8b4473d295e9
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-29 06:03:38 +0530

    Update StudentTile.jsx

--- src/widgets/StudentTile.jsx
+++ src/widgets/StudentTile.jsx
@@ -9,8 +9,8 @@ StudentTile.propTypes = {
 function StudentTile({ id }) {
     return (
         <div className="w-64 h-64 border-2 border-black flex flex-col justify-center items-center m-4 tile">
-            <h2 className="text-2xl course-code tile-text">{registration_number}</h2>
-            <h2 className="text-2xl course-code tile-text">{name}</h2>
+            <h2 className="text-2xl course-code tile-text">{student.registration_number}</h2>
+            <h2 className="text-2xl course-code tile-text">{student.name}</h2>
         </div>
     );
 }
