ecc1a46b78c817b29e422dbd3628c6db8aca8cc5
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-29 06:25:46 +0530

    Update Course.jsx

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -39,7 +39,7 @@ const Course = () => {
         <div className='flex flex-wrap justify-center items-start student-tiles'>
             {students.map((student) => (
                 <Link to={`/student/${student.id}`} key={student.id}>
-                    <StudentTile registration_number={student.registration_number} name={student.name} />
+                    <StudentTile id={student.id} />
                 </Link>
             ))}
         </div>
