8cd9dc2c4f51e336162752f9a0e5d5f9216ed4a0
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-29 08:13:19 +0530

    Update CourseTile.jsx

--- src/widgets/CourseTile.jsx
+++ src/widgets/CourseTile.jsx
@@ -1,40 +1,16 @@
 import PropTypes from 'prop-types';
-import { useEffect, useState } from 'react';
 import './CourseTile.css';
 
 CourseTile.propTypes = {
-    id: PropTypes.number.isRequired,
+    courseCode: PropTypes.string.isRequired,
+    academicYear: PropTypes.string.isRequired,
 };
 
-function CourseTile({ id }) {
-    const [course, setCourse] = useState({ code: '', academic_year: '' });
-
-    useEffect(() => {
-        fetch('http://127.0.0.1:8000/api/course/get-by-id', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ cid: id }),
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            setCourse(data.data);
-        })
-        .catch(error => {
-            console.error('There was an error!', error);
-        });
-    }, [id]);
-
+function CourseTile({ courseCode, academicYear }) {
     return (
         <div className="w-64 h-64 border-2 border-black flex flex-col justify-center items-center m-4 tile">
-            <h2 className="text-2xl course-code tile-text">{course.code}</h2>
-            <p className="text-lg academic-year tile-text">{course.academic_year}</p>
+            <h2 className="text-2xl course-code tile-text">{courseCode}</h2>
+            <p className="text-lg academic-year tile-text">{academicYear}</p>
         </div>
     );
 }
