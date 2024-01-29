abb65059c2960fd18846bb7495e129220e4eae63
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-29 08:10:24 +0530

    Update CourseTile.jsx

--- src/widgets/CourseTile.jsx
+++ src/widgets/CourseTile.jsx
@@ -8,8 +8,8 @@ CourseTile.propTypes = {
 function CourseTile({ id }) {
     return (
         <div className="w-64 h-64 border-2 border-black flex flex-col justify-center items-center m-4 tile">
-            <h2 className="text-2xl course-code tile-text">{courseCode}</h2>
-            <p className="text-lg academic-year tile-text">{academicYear}</p>
+            <h2 className="text-2xl course-code tile-text">{course.courseCode}</h2>
+            <p className="text-lg academic-year tile-text">{course.academicYear}</p>
         </div>
     );
 }
